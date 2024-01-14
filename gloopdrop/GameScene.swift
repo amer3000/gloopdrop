15a0571faa042d7097edde66e9fd4b9c3f4c93c8
Amer Khalid <amer@Amers-Mac-mini.local>
2024-01-14 11:41:05 -0600

    chapter 1 - background

--- gloopdrop/GameScene.swift
+++ gloopdrop/GameScene.swift
@@ -5,4 +5,20 @@
 //  Created by Amer Khalid on 11/1/23.
 //
 
-import Foundation
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background_1")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.position = CGPoint(x: 0, y: 0)
+        addChild(background)
+        
+        // foreground
+        let foreground = SKSpriteNode(imageNamed: "foreground_1")
+        foreground.anchorPoint = CGPoint(x: 0, y: 0)
+        foreground.position = CGPoint(x: 0, y: 0)
+        addChild(foreground)
+    }
+}
