7ccf01f2b6c05eb87edf9308048b59e78d5b1937
Amer Khalid <amer@Amers-Mac-mini.local>
2024-03-04 15:15:49 -0600

    done until page 65

--- gloopdrop/GameScene.swift
+++ gloopdrop/GameScene.swift
@@ -12,6 +12,11 @@ class GameScene: SKScene {
 
     let player = Player()
     let playerSpeed: CGFloat = 1.5
+    var level: Int = 1
+    var numberOfDrops: Int = 10
+    var dropSpeed: CGFloat = 1.0
+    var minDropSpeed: CGFloat = 0.12
+    var maxDropSpeed: CGFloat = 1.0
 
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background_1")
@@ -38,10 +43,31 @@ class GameScene: SKScene {
     // MARK: - GAME FUNCTIONS
 
     func spawnMultipleGloops() {
-        let wait = SKAction.wait(forDuration: TimeInterval(1.0))
+
+        switch level {
+        case 1, 2, 3, 4, 5:
+            numberOfDrops = level * 10
+        case 6:
+            numberOfDrops = 75
+        case 7:
+            numberOfDrops = 100
+        case 8:
+            numberOfDrops = 150
+        default:
+            numberOfDrops = 150
+        }
+
+        dropSpeed = 1 / (CGFloat(level) + (CGFloat(level) / CGFloat(numberOfDrops)))
+        if dropSpeed < minDropSpeed {
+            dropSpeed = minDropSpeed
+        } else if dropSpeed > maxDropSpeed {
+            dropSpeed = maxDropSpeed
+        }
+
+        let wait = SKAction.wait(forDuration: TimeInterval(dropSpeed))
         let spawn = SKAction.run { [unowned self] in self.spawnGloop() }
         let sequence = SKAction.sequence([wait, spawn])
-        let repeatAction = SKAction.repeat(sequence, count: 10)
+        let repeatAction = SKAction.repeat(sequence, count: numberOfDrops)
 
         run(repeatAction, withKey: "gloop")
     }
